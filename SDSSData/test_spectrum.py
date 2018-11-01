fc7e318b6d63ddfc76b0ee41a637980d9763f870
Kathryn Napier <278559c@curtin.edu.au>
2018-11-01 14:46:38 +0800

    actual final test

--- SDSSData/test_spectrum.py
+++ SDSSData/test_spectrum.py
@@ -9,7 +9,8 @@ filename = "spec-10000-57346-0002.fits"
 def test_hdu():
 	s = Spectrum(filename)
 	assert len(s.datafile) == 4, "unexpected number of HDUs in file"
-	
+
+# check ra value makes sense 	
 def test_ra():
-	s = Spectrum(filename)
-	np.testing.assert_approx_equal(s.ra(), 29.815837)
+	s= Spectrum(filename)
+	assert 0 < s.ra() < 360 
