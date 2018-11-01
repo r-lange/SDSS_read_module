7de2800877282091b27ea74af63a5bc109884310
Rebecca L <becca.lange84@gmail.com>
2018-11-01 12:11:47 +0800

    intial draft of spectrum class

---
+++ SDSSData/spectrum.py
@@ -0,0 +1,29 @@
+import .errors 
+from astropy.io import fits
+
+# ========================
+# Define spectrum class
+# ========================
+
+class Spectrum(Object):
+	def __init__(self, filepath = None):
+		if filepath == None:
+			raise SDSSFileNotSpecified("A spectrum file must be specified.")
+		self.filepath = filepath
+		self.datafile = fits.open(self.filepath)
+		self._ra = None
+	
+	def _isValid(self, file=None):
+		# check file is valid
+		if len(self.datafile) != 4:
+			raise HDUError("The number of HDU is not 4")
+	
+	def ra(self):
+		''' Returns the RA of this spectrum in degrees'''
+		if self._ra == None:
+			hdr = self.datafile[0].header
+			# check if key exists
+			if not "RA" in list(hdr.keys()):
+				raise HDUKeyError("Key does not exist in header")
+			self._ra = hdr['RA']
+		return(self._ra)
