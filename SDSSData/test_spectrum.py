7965234c0d67b98c1a73e676df0e196e4d0632a7
Kathryn Napier <278559c@curtin.edu.au>
2018-11-01 13:33:21 +0800

    moving unit test

---
+++ SDSSData/test_spectrum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import numpy as np
+from SDSSData import Spectrum
+
+filename = "spec-10000-57346-0002.fits"
+
+# check we can read spectrum file, has 4 HDUs
+def test_hdu():
+	s = Spectrum(filename)
+	assert len(s.hdu_list) == 4, "unexpected number of HDUs in file"
+	
+def test_ra():
+	s = Spectrum(filename)
+	np.testing.assert_approx_equal(s.ra, 12.3432)
